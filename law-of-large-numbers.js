63eb5ee055fda44c16dc48a332cfad0d74369a85
박도영 <doyeongmath@gmail.com>
2025-08-22 12:01:52 +0900

    Update law-of-large-numbers.js

--- law-of-large-numbers.js
+++ law-of-large-numbers.js
@@ -74,7 +74,8 @@ function drawChart(data, p, h) {
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false,
+      maintainAspectRatio: true,
+      aspectRatio: 2.5,
       plugins: {
         title: {
           display: true,
@@ -160,7 +161,8 @@ function drawTheoryChart() {
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false,
+      maintainAspectRatio: true,
+      aspectRatio: 2.5,
       plugins: {
         title: {
           display: true,
